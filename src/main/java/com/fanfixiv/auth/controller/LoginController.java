558c48d26d7347e56b571c744998cd76d6f8e3f3
Roharui <jack555586@gmail.com>
2022-09-18 18:10:25 +0900

    feat: GET /profile API 추가
    ProfileResultDto 추가

--- src/main/java/com/fanfixiv/auth/controller/LoginController.java
+++ src/main/java/com/fanfixiv/auth/controller/LoginController.java
@@ -2,9 +2,12 @@ package com.fanfixiv.auth.controller;
 
 import com.fanfixiv.auth.dto.login.LoginDto;
 import com.fanfixiv.auth.dto.login.LoginResultDto;
+import com.fanfixiv.auth.dto.profile.ProfileResultDto;
 import com.fanfixiv.auth.service.LoginService;
-
+import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
-  @Autowired
-  LoginService loginService;
+  @Autowired LoginService loginService;
 
   @PostMapping("/login")
   public LoginResultDto login(@RequestBody LoginDto loginDto) throws Exception {
     return loginService.doLogin(loginDto);
+  }
+
+  @GetMapping("/profile")
+  public ProfileResultDto profile(Authentication authentication, Principal principal)
+      throws Exception {
+    return new ProfileResultDto(principal.getName());
   }
 }
