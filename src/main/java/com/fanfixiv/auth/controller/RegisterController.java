7bbd594c3d8b5550206dca4a8ccf902f9c1daad4
Roharui <jack555586@gmail.com>
2022-09-19 11:35:03 +0900

    fix: 로그인 후 중복확인이 가능한 로직 수정

--- src/main/java/com/fanfixiv/auth/controller/RegisterController.java
+++ src/main/java/com/fanfixiv/auth/controller/RegisterController.java
@@ -9,13 +9,15 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Validated
+@RequestMapping("/register")
 public class RegisterController {
-  @PostMapping("/register")
+  @PostMapping("/")
   public RegisterResultDto register(@RequestBody @Valid RegisterDto registerDto) {
     return new RegisterResultDto();
   }
