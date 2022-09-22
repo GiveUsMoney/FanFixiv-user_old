cf74445ef18bf3f67fd1c215c95e6be2390a7fc6
Roharui <jack555586@gmail.com>
2022-09-22 12:05:38 +0900

    test: 테스트시 h2 DB 연결 설정 추가
    h2 DB 연결 확인용 테스트 추가

--- src/test/java/com/fanfixiv/auth/AuthE2ETests.java
+++ src/test/java/com/fanfixiv/auth/AuthE2ETests.java
@@ -4,14 +4,19 @@ import static io.restassured.RestAssured.*;
 // import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import com.fanfixiv.auth.entity.ProfileEntity;
+import com.fanfixiv.auth.repository.ProfileRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthE2ETests {
+
+  @Autowired private ProfileRepository ProfileRepository;
 
   @LocalServerPort private int _port;
 
@@ -29,5 +34,13 @@ public class AuthE2ETests {
         .assertThat()
         .body("content", equalTo("Hello World!!!"))
         .body("id", equalTo(0));
+  }
+
+  @Test
+  @DisplayName("H2 데이터 베이스 테스트")
+  void h2test() {
+    ProfileEntity profile = ProfileEntity.builder().nickname("테스트").descript("테스트입니다.").build();
+
+    ProfileRepository.save(profile);
   }
 }
