b883b70ac6accea24df0b7c5a48d01592f09b5d3
Roharui <jack555586@gmail.com>
2022-10-05 13:47:42 +0900

    fix: ProfileResultDto에 birth property 추가

--- src/main/java/com/fanfixiv/auth/dto/profile/ProfileResultDto.java
+++ src/main/java/com/fanfixiv/auth/dto/profile/ProfileResultDto.java
@@ -1,5 +1,6 @@
 package com.fanfixiv.auth.dto.profile;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.fanfixiv.auth.entity.UserEntity;
@@ -13,6 +14,7 @@ public class ProfileResultDto {
   private String email;
   private String nickname;
   private LocalDateTime nn_md_date;
+  private LocalDate birth;
   private String profile_img;
   private String descript;
   private boolean is_tr;
@@ -21,6 +23,7 @@ public class ProfileResultDto {
     this.email = user.getEmail();
     this.nickname = user.getProfile().getNickname();
     this.nn_md_date = user.getProfile().getNn_md_date();
+    this.birth = user.getProfile().getBirth();
     this.profile_img = user.getProfile().getProfile_img();
     this.descript = user.getProfile().getDescript();
     this.is_tr = user.getProfile().is_tr();
