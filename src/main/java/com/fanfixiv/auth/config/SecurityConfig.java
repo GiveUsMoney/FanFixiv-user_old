dd33576146059e4a950ec1c510a3995ca645ee40
Roharui <jack555586@gmail.com>
2022-10-07 13:45:19 +0900

    fix: cors 에러 해결
    setAllowCredentials = true일때 origin이 *이면 안되므로 origin을 따로 설정함.

--- src/main/java/com/fanfixiv/auth/config/SecurityConfig.java
+++ src/main/java/com/fanfixiv/auth/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.fanfixiv.auth.config;
 import com.fanfixiv.auth.filter.JwtAuthenticationFilter;
 import com.fanfixiv.auth.handler.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -42,14 +45,13 @@ public class SecurityConfig {
   }
 
   @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
+  CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-
-    configuration.addAllowedOrigin("*");
-    configuration.addAllowedHeader("*");
-    configuration.addAllowedMethod("*");
+    configuration.setAllowedOrigins(
+        Arrays.asList("http://localhost:3000/", "https://giveusmoney.github.io/"));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
     configuration.setAllowCredentials(true);
-
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
@@ -60,6 +62,7 @@ public class SecurityConfig {
 
     http.httpBasic().disable(); // Http Basic을 이용한 보안 해제
     http.formLogin().disable(); // 기본 Login Form 해제
+    http.cors();
     http.csrf().disable(); // CSRF 보안 해제
     http.sessionManagement()
         .sessionCreationPolicy(
@@ -69,7 +72,7 @@ public class SecurityConfig {
         .permitAll()
         .antMatchers("/login", "/register/**", "/", "/refresh")
         .permitAll(); // 로그인 회원가입은 보안 해제
-    
+
     http.authorizeRequests()
         .antMatchers("/roles/user")
         .hasRole("USER")
@@ -79,11 +82,11 @@ public class SecurityConfig {
         .hasRole("TRANSLATER")
         .antMatchers("/roles/admin")
         .hasRole("ADMIN");
-        
+
     http.authorizeRequests()
         .anyRequest()
         .authenticated();
-  
+
     http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
 
     http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
