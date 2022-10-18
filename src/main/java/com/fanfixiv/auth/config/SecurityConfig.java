78e7ad2919051317e08a82fe5d7b52565f00922c
Roharui <jack555586@gmail.com>
2022-10-18 20:01:08 +0900

    refactor: Arrays.asList로 리스트 초기화 리팩토링
    개발 서버 URL CORS 허용 origin에 추가

--- src/main/java/com/fanfixiv/auth/config/SecurityConfig.java
+++ src/main/java/com/fanfixiv/auth/config/SecurityConfig.java
@@ -48,7 +48,10 @@ public class SecurityConfig {
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(
-        Arrays.asList("http://localhost:3000/", "https://giveusmoney.github.io/"));
+        Arrays.asList(
+            "http://localhost:3000/",
+            "https://giveusmoney.github.io/",
+            "https://aumlaytno6.execute-api.ap-northeast-2.amazonaws.com/"));
     configuration.setAllowedMethods(Arrays.asList("*"));
     configuration.setAllowedHeaders(Arrays.asList("*"));
     configuration.setAllowCredentials(true);
