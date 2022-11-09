39d406709661a5836046a4ac2da5cb515969687a
Roharui <jack555586@gmail.com>
2022-11-09 12:33:27 +0900

    fix: CustomAuthenticationEntryPoint에서 exception 처리 불가 에러 수정

--- src/main/java/com/fanfixiv/auth/service/CustomUserDetailService.java
+++ src/main/java/com/fanfixiv/auth/service/CustomUserDetailService.java
@@ -3,8 +3,6 @@ package com.fanfixiv.auth.service;
 import com.fanfixiv.auth.details.User;
 import com.fanfixiv.auth.entity.UserEntity;
 
-import com.fanfixiv.auth.exception.UnauthorizedException;
-
 import com.fanfixiv.auth.repository.jpa.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +11,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,11 +22,11 @@ public class CustomUserDetailService implements UserDetailsService {
 
   @Override
   @Transactional
-  public UserDetails loadUserByUsername(String _id) throws UnauthorizedException {
+  public UserDetails loadUserByUsername(String _id) throws UsernameNotFoundException {
     Long id = Long.parseLong(_id);
     UserEntity user = userRepository
         .findById(id)
-        .orElseThrow(() -> new UnauthorizedException("사용자를 찾을수 없습니다."));
+        .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을수 없습니다."));
 
     return new User(user);
   }
