d4c1fbfdce6200dbf851e605f109a276151d36a6
Roharui <jack555586@gmail.com>
2022-12-10 14:44:56 +0900

    feat: 관리자 서버를 위한 BaseEntity의 변경

--- src/main/java/com/fanfixiv/auth/entity/BaseEntity.java
+++ src/main/java/com/fanfixiv/auth/entity/BaseEntity.java
@@ -4,11 +4,13 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -20,14 +22,16 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseEntity {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   protected long seq;
 
   @CreatedDate
-  @Column(updatable = false, nullable = false)
+  @ColumnDefault("now()")
+  @Column(updatable = false, nullable = false) 
   protected LocalDateTime createdAt;
 
   @LastModifiedDate
+  @ColumnDefault("now()")
   @Column(nullable = false)
   protected LocalDateTime updatedAt;
 }
